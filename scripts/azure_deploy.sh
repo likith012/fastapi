ca2ae3661822b8ed455c606e2ad4e08574ceddf1
likith012 <likith012@gmail.com>
2023-05-12 21:53:52 +0530

    🐞 fix:

--- scripts/azure_deploy.sh
+++ scripts/azure_deploy.sh
@@ -1,6 +1,6 @@
 # Update package list and install Python 3 pip package manager
 sudo apt-get update
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3 python3-pip
 
 # Create a virtual environment
 python -m venv venv
